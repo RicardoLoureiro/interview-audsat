e93437b07ee79b8a5589835b253b90bdd93fac23
Ricardo Hernandez <=>
2023-05-21 14:35:06 -0300

    fix: migration tables

--- src/main/resources/db/migration/V1__0_create_table_cars.sql
+++ src/main/resources/db/migration/V1__0_create_table_cars.sql
@@ -1,8 +1,8 @@
 CREATE TABLE cars (
     id int NOT NULL AUTO_INCREMENT,
-    fipe_value decimal(19,2) NOT NULL,
     model varchar(100) DEFAULT NULL,
     manufacturer varchar(19) DEFAULT NULL,
+    fipe_value decimal(19,2) NOT NULL,
     year varchar(7) DEFAULT NULL,
     PRIMARY KEY (id)
 );
