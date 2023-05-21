8525025c8b9bb963edcc98749efd59fd529fade2
Ricardo Hernandez <=>
2023-05-21 16:18:22 -0300

    chore: adjust migrations

---
+++ src/main/resources/db/migration/V1_0_0__create_table_cars.sql
@@ -0,0 +1,8 @@
+CREATE TABLE cars (
+    `id` bigint NOT NULL AUTO_INCREMENT,
+    `model` varchar(100) DEFAULT NOT NULL,
+    `manufacturer` varchar(19) DEFAULT NOT NULL,
+    `fipe_value` decimal(19,2) NOT NULL,
+    `year` varchar(7) DEFAULT NULL,
+    PRIMARY KEY (id)
+);
--- src/main/resources/db/migration/V1__0_create_table_cars.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE cars (
-    id int NOT NULL AUTO_INCREMENT,
-    model varchar(100) DEFAULT NULL,
-    manufacturer varchar(19) DEFAULT NULL,
-    fipe_value decimal(19,2) NOT NULL,
-    year varchar(7) DEFAULT NULL,
-    PRIMARY KEY (id)
-);
